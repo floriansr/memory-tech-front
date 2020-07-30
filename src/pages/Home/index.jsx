b42e9ca9e43aae95dd279293fbf4a77d8d5fa16b
floriansr <sueurfloriansf@gmail.com>
2020-07-30 12:09:37 +0200

    [add] useEffect fetch datas Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import APIManager from 'services/APIManager';
+import { Container, Div, Row, Col, Text } from 'atomize';
+
+import { setTransactions } from '../../redux';
 
 const Home = () => {
-  return <>Homepage</>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const allRevenues = async () => {
+      const res = await APIManager.allRevenues();
+      console.log('Home -> res', res);
+      dispatch(setTransactions(res.revenues));
+    };
+    allRevenues();
+  }, [dispatch]);
+
+  return (
+    <>
+      <Container>
+        <Div>
+          <Row>
+            <Text tag="h3">COUNTRY</Text>
+          </Row>
+        </Div>
+        <Div>
+          <Col>
+            <Text tag="h3">SUMMARY</Text>
+          </Col>
+        </Div>
+        <Div>
+          <Col>
+            <Text tag="h3">REVENUE PER MONTH</Text>
+          </Col>
+        </Div>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
