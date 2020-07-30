0aade8b105b5511cedc134e64c59f28188c8891a
floriansr <sueurfloriansf@gmail.com>
2020-07-30 15:19:07 +0200

    [fix] basic dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,37 +1,51 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Dropdown, Anchor, Div, Icon } from 'atomize';
 import shortId from 'shortid';
 
 const BasicDropdown = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const { countries } = useSelector((state) => state.transaction);
+
   const menuList = (
     <Div p={{ x: '1rem', y: '0.5rem' }}>
-      {['France', 'United Kingdom', 'All'].map((name) => (
-        <Anchor d="block" p={{ y: '0.25rem' }} key={shortId.generate()}>
+      {countries.map((name) => (
+        <Anchor
+          href={`/country/${name}`}
+          d="block"
+          p={{ y: '0.25rem' }}
+          key={shortId.generate()}
+        >
           {name}
         </Anchor>
       ))}
     </Div>
   );
 
+  console.log('BasicDropdown -> countries.length', countries.length);
+
   return (
     <>
-      <Dropdown
-        w="50%"
-        isOpen={showDropdown}
-        onClick={() => setShowDropdown(!showDropdown)}
-        menu={menuList}
-        bg="info100"
-        focusBg="info200"
-        borderColor="info600"
-        focusBorderColor="info800"
-        textColor="info800"
-        textWeight="500"
-        openSuffix={<Icon name="Up" color="info700" size="16px" />}
-        closeSuffix={<Icon name="Down" color="info700" size="16px" />}
-      >
-        All
-      </Dropdown>
+      {countries.length === 0 ? (
+        ''
+      ) : (
+        <Dropdown
+          w="50%"
+          isOpen={showDropdown}
+          onClick={() => setShowDropdown(!showDropdown)}
+          menu={menuList}
+          bg="info100"
+          focusBg="info200"
+          borderColor="info600"
+          focusBorderColor="info800"
+          textColor="info800"
+          textWeight="500"
+          openSuffix={<Icon name="Up" color="info700" size="16px" />}
+          closeSuffix={<Icon name="Down" color="info700" size="16px" />}
+        >
+          All
+        </Dropdown>
+      )}
     </>
   );
 };
