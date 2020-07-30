454c1ffe7a445e060f3742ea0048e8f6fe4d5f0b
floriansr <46346593+floriansr@users.noreply.github.com>
2020-07-30 11:40:56 +0200

    Styletron engine atomic antd (#6)
    * [add] atomic styletron engine packages
    * [add] Styletron & Router DOM
    * [add] antd css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Home from 'pages/Home';
+
+const debug =
+  process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
 
 const App = () => {
   return (
     <>
-      <Home />
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <Router>
+          <Switch>
+            <Home exact path="/" component={Home} />
+            <Route path="*" status={404} />
+          </Switch>
+        </Router>
+      </StyletronProvider>
     </>
   );
 };
